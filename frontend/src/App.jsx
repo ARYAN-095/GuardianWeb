f6b77aafc29f42bd2f29004ac1e9f93cd69771d2
Aryan095 <aryanshukla095@gmail.com>
2025-08-19 01:49:44 +0530

    update url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,9 @@ const Wrench = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="
 const Loader = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="animate-spin h-5 w-5"><path d="M21 12a9 9 0 1 1-6.219-8.56"></path></svg>;
 
 // --- API CONFIG ---
-const API_URL = "http://127.0.0.1:8000"; 
+// const API_URL = "http://127.0.0.1:8000"; 
+const API_URL = "https://guardianweb-3.onrender.com/"; 
+
 
 // --- HELPER FUNCTIONS ---
 const getSeverityClass = (severity) => {
